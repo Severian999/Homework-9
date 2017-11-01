eea30ba1db66a96502d94fd14f1ce558307fbde4
Don Gass <don.gass@gmail.com>
2017-11-01 16:53:51 -0600

    Add files via upload

---
+++ DonGass_hw9_task1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  1 16:11:44 2017
+
+@author: Don Gass
+"""
+
+import matplotlib.pyplot as plt
+
+money_spent = list(range(20, 101))
+total_bars = [(n + (n-1)//9) for n in money_spent]
+total_cost = [(x/total_bars[i]) for i, x in enumerate(money_spent)]
+
+plt.plot(money_spent, total_cost, label="Averge cost of chocolate bar")
+plt.xlabel("Money spent (in $)")
+plt.ylabel("Average cost / bar")
+plt.title("Average Cost of Chocolate Bar")
+plt.legend(shadow=True, loc="upper right")
+plt.savefig("task1_plot.png", dpi=300)
+
+plt.show()
